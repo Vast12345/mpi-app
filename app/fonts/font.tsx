0733943d2b0de215b2b952c720de8bda54047dc0
Vast12345 <vastprogramming@gmail.com>
2024-11-28 13:04:59 -0500

    feat: :bug: Fixed errors for deployment.
    Erased ununed imports and fonts.

--- app/fonts/font.tsx
+++
@@ -1,7 +0,0 @@
-import { Inter } from 'next/font/google';
-
-const inter = Inter({
-    weight: ['400', '500', '600'],
-    subsets: ['latin'],
-    variable: '--inter-font'
-})
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="text-black-800 font-extrabold">Hello</div>
